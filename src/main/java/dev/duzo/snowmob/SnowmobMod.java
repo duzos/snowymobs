f4d4a5759b37135b1ee2186a29e6cadf33a1cc79
James Hall <james.hall01@outlook.com>
2025-02-27 23:13:26 +0000

    chore: setup project

---
+++ src/main/java/dev/duzo/snowmob/SnowmobMod.java
@@ -0,0 +1,38 @@
+package dev.duzo.snowmob;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(SnowmobMod.MODID)
+public class SnowmobMod {
+	public static final String MODID = "snowmob";
+	private static final Logger LOGGER = LogUtils.getLogger();
+
+	public SnowmobMod() {
+		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	private void commonSetup(final FMLCommonSetupEvent event) {
+
+	}
+
+	// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+	@Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+	public static class ClientModEvents {
+
+		@SubscribeEvent
+		public static void onClientSetup(FMLClientSetupEvent event) {
+
+		}
+	}
+}
