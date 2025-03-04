51c445cfd887741de834545c3c0bc1c008051eca
James Hall <james.hall01@outlook.com>
2025-03-04 01:21:05 +0000

    fix: snow level not being synched

--- src/main/java/dev/duzo/snowmob/mixin/LivingEntityMixin.java
+++ src/main/java/dev/duzo/snowmob/mixin/LivingEntityMixin.java
@@ -2,6 +2,9 @@ package dev.duzo.snowmob.mixin;
 
 import dev.duzo.snowmob.api.SnowCollecting;
 import dev.duzo.snowmob.api.SnowData;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,22 +16,26 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin implements SnowCollecting {
 	@Unique
-	private int snowmob$snowLevel = 0;
+	private static final EntityDataAccessor<Integer> SNOW_LEVEL = SynchedEntityData.defineId(LivingEntity.class, EntityDataSerializers.INT);
 
 	@Inject(method = "readAdditionalSaveData", at = @At("TAIL"))
 	public void snowmob$readAdditionalSaveData(net.minecraft.nbt.CompoundTag tag, CallbackInfo ci) {
-		snowmob$snowLevel = tag.getInt("SnowLevel");
+		setSnowLevel(tag.getInt("SnowLevel"));
 	}
 
 	@Inject(method = "addAdditionalSaveData", at = @At("TAIL"))
 	public void snowmob$addAdditionalSaveData(net.minecraft.nbt.CompoundTag tag, CallbackInfo ci) {
-		tag.putInt("SnowLevel", snowmob$snowLevel);
+		tag.putInt("SnowLevel", getSnowLevel());
 	}
 
+	@Inject(method = "defineSynchedData", at = @At("TAIL"))
+	public void snowmob$defineSynchedData(CallbackInfo ci) {
+		((LivingEntity) (Object) this).getEntityData().define(SNOW_LEVEL, 0);
+	}
 
 	@Override
 	public int getSnowLevel() {
-		return snowmob$snowLevel;
+		return ((LivingEntity) (Object) this).getEntityData().get(SNOW_LEVEL);
 	}
 
 	@Override
@@ -38,8 +45,8 @@ public class LivingEntityMixin implements SnowCollecting {
 
 	@Override
 	public boolean setSnowLevel(int level) {
-		snowmob$snowLevel = Mth.clamp(level, 0, getMaxSnow());
+		((LivingEntity) (Object) this).getEntityData().set(SNOW_LEVEL, Mth.clamp(level, 0, getMaxSnow()));
 
-		return snowmob$snowLevel == level;
+		return getSnowLevel() == level;
 	}
 }
