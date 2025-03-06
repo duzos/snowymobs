26b6201248269de6e22de46bae5376426e766488
James Hall <james.hall01@outlook.com>
2025-03-06 22:28:41 +0000

    todo: alpha

--- src/main/java/dev/duzo/snowmob/api/SnowData.java
+++ src/main/java/dev/duzo/snowmob/api/SnowData.java
@@ -16,10 +16,11 @@ import net.minecraftforge.server.ServerLifecycleHooks;
 import java.util.List;
 import java.util.Optional;
 
-public record SnowData(EntityType<?> type, List<ResourceLocation> textures) {
+public record SnowData(EntityType<?> type, List<ResourceLocation> textures, int maxLevel) {
 	public static final Codec<SnowData> CODEC = RecordCodecBuilder.create(instance -> instance.group(
 			EntityTypeCodec.CODEC.fieldOf("type").forGetter(SnowData::type),
-			ResourceLocation.CODEC.listOf().fieldOf("textures").forGetter(SnowData::textures)
+			ResourceLocation.CODEC.listOf().fieldOf("textures").forGetter(SnowData::textures),
+			Codec.INT.optionalFieldOf("maxLevel").forGetter(data -> Optional.of(data.maxLevel()))
 	).apply(instance, SnowData::new));
 
 	public static final ResourceKey<Registry<SnowData>> REGISTRY_KEY = ResourceKey.createRegistryKey(new ResourceLocation(SnowmobMod.MODID, "snow_data"));
@@ -27,6 +28,32 @@ public record SnowData(EntityType<?> type, List<ResourceLocation> textures) {
 
 	public static Registry<SnowData> getRegistry() {
 		return ServerLifecycleHooks.getCurrentServer().registryAccess().registry(REGISTRY_KEY).orElse(null);
+	}
+
+	public SnowData(EntityType<?> type, List<ResourceLocation> textures) {
+		this(type, textures, textures.size() * 3);
+	}
+
+	public SnowData(EntityType<?> type, List<ResourceLocation> textures, Optional<Integer> maxLevel) {
+		this(type, textures, maxLevel.orElse(textures.size() * 3));
+	}
+
+	public static Optional<SnowData> get(LivingEntity e) {
+		return get(e.getType());
+	}
+
+	public static int getMaxLevel(LivingEntity entity) {
+		Registry<SnowData> registry = getRegistry();
+		if (registry == null) {
+			return 0;
+		}
+
+		SnowData data = get(entity.getType()).orElse(null);
+		if (data == null) {
+			return 0;
+		}
+
+		return data.maxLevel();
 	}
 
 	public static Optional<SnowData> get(EntityType<?> type) {
@@ -44,6 +71,18 @@ public record SnowData(EntityType<?> type, List<ResourceLocation> textures) {
 		return Optional.empty();
 	}
 
+	public Optional<ResourceLocation> texture(LivingEntity entity) {
+		if (entity.getType() != type) {
+			throw new IllegalStateException("Entity type does not match snow data type");
+		}
+
+		int level = ((SnowCollecting) entity).getSnowLevel() - 1;
+		if (level <= 0) return Optional.empty();
+
+		int index = (maxLevel() - level) % textures.size();
+		return Optional.of(textures.get(index));
+	}
+
 	public static Optional<List<ResourceLocation>> textures(LivingEntity entity) {
 		Registry<SnowData> registry = getRegistry();
 		if (registry == null) {
@@ -58,40 +97,14 @@ public record SnowData(EntityType<?> type, List<ResourceLocation> textures) {
 		return Optional.of(data.textures());
 	}
 
-	public static Optional<ResourceLocation> texture(LivingEntity entity) {
-		Registry<SnowData> registry = getRegistry();
-		if (registry == null) {
-			return Optional.empty();
+	public float alpha(LivingEntity entity) {
+		if (entity.getType() != type) {
+			throw new IllegalStateException("Entity type does not match snow data type");
 		}
 
-		SnowData data = get(entity.getType()).orElse(null);
-		if (data == null) {
-			return Optional.empty();
-		}
+		int level = ((SnowCollecting) entity).getSnowLevel();
+		if (level <= 0) return 0;
 
-		int level = getLevel(entity) - 1;
-		if (level < 0 || level >= data.textures().size()) {
-			return Optional.empty();
-		}
-
-		return Optional.of(data.textures().get(level));
-	}
-
-	public static int getLevel(LivingEntity entity) {
-		return ((SnowCollecting) entity).getSnowLevel();
-	}
-
-	public static int getMaxLevel(LivingEntity entity) {
-		Registry<SnowData> registry = getRegistry();
-		if (registry == null) {
-			return 0;
-		}
-
-		SnowData data = get(entity.getType()).orElse(null);
-		if (data == null) {
-			return 0;
-		}
-
-		return data.textures().size();
+		return (float) level / maxLevel();
 	}
 }
--- src/main/java/dev/duzo/snowmob/mixin/client/LivingEntityRendererMixin.java
+++ src/main/java/dev/duzo/snowmob/mixin/client/LivingEntityRendererMixin.java
@@ -13,6 +13,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -22,6 +23,8 @@ public abstract class LivingEntityRendererMixin<T extends LivingEntity, M extend
 	protected M model;
 	@Unique
 	private boolean snowmob$renderingSnow = false;
+	@Unique
+	private T snowmob$entityCache;
 
 	@Shadow
 	public abstract void render(T p_115308_, float p_115309_, float p_115310_, PoseStack p_115311_, MultiBufferSource p_115312_, int p_115313_);
@@ -30,9 +33,11 @@ public abstract class LivingEntityRendererMixin<T extends LivingEntity, M extend
 	public void snowmob$render(T p_114485_, float p_114486_, float p_114487_, PoseStack p_114488_, MultiBufferSource p_114489_, int p_114490_, CallbackInfo ci) {
 		if (!(snowmob$renderingSnow)) {
 			snowmob$renderingSnow = true;
+			snowmob$entityCache = p_114485_;
 			this.render(p_114485_, p_114486_, p_114487_, p_114488_, p_114489_, p_114490_);
 		}
 
+		snowmob$entityCache = null;
 		snowmob$renderingSnow = false;
 	}
 
@@ -40,9 +45,23 @@ public abstract class LivingEntityRendererMixin<T extends LivingEntity, M extend
 	public void snowmob$getRenderType(T entity, boolean p_115323_, boolean p_115324_, boolean p_115325_, CallbackInfoReturnable<RenderType> cir) {
 		if (!snowmob$renderingSnow) return;
 
-		ResourceLocation texture = SnowData.texture(entity).orElse(null);
+		SnowData data = SnowData.get(entity).orElse(null);
+		if (data == null) return;
+		ResourceLocation texture = data.texture(entity).orElse(null);
 		if (texture == null) return;
 
 		cir.setReturnValue(this.model.renderType(texture));
+	}
+
+	@ModifyArg(index = 7, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/EntityModel;renderToBuffer(Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;IIFFFF)V"), method = "render(Lnet/minecraft/world/entity/LivingEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V")
+	public float snowmob$modifyAlpha(float alpha) {
+		if (!(snowmob$renderingSnow) || snowmob$entityCache == null) return alpha;
+
+		SnowData data = SnowData.get(snowmob$entityCache).orElse(null);
+		if (data == null) return alpha;
+
+		System.out.println(data.alpha(snowmob$entityCache));
+
+		return data.alpha(snowmob$entityCache); // @todo doesnt work properly
 	}
 }
